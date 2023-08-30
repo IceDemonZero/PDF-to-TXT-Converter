9e8d07e7205dfb8bb9cae6dcf5f56735342af5af
Ace Edwards <thanoseid@outlook.com>
2023-08-29 17:10:26 -0700

    Initial commit

---
+++ constants.py
@@ -0,0 +1,4 @@
+# constants.py
+TESS_PATH = r'D:\PycharmProjects\Tesseract-OCR\tesseract.exe'  # path of the tesseract. PLEASE change this before using.
+POPPLER_PATH = r"D:\PycharmProjects\Poppler\bin"  # path of poppler. PLEASE change this when using it.
+DEFAULT_SAVE_PATH = "output.txt"  # this is used when a file name for the .txt converted from a pdf cannot be found.
